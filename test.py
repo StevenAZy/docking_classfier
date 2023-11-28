24531a0367e5ec2d77563cbcd66efc49c114da38
StevenAZy <1315657491@qq.com>
2023-11-28 18:35:21 +0800

    init repo

---
+++ test.py
@@ -0,0 +1,31 @@
+from graphein.protein.config import ProteinGraphConfig
+from graphein.protein.graphs import construct_graph
+from graphein.protein.visualisation import plotly_protein_structure_graph
+from graphein.rna.config import RNAGraphConfig
+from graphein.rna.graphs import construct_rna_graph_3d
+from graphein.rna.visualisation import plotly_rna_structure_graph
+from graphein.rna.edges import (
+    add_all_dotbracket_edges,
+    add_pseudoknots,
+    add_phosphodiester_bonds,
+    add_base_pairing_interactions
+)
+
+# p_config = ProteinGraphConfig()
+# g_p = construct_graph(config=p_config, path='data/1ASY_r_u.pdb')
+
+# p_1 = plotly_protein_structure_graph(
+#     g_1,
+#     colour_edges_by="kind",
+#     colour_nodes_by="degree",
+#     label_node_ids=False,
+#     plot_title="Peptide backbone graph. Nodes coloured by degree.",
+#     node_size_multiplier=1
+#     )
+# p_1.show()
+
+r_config = RNAGraphConfig()
+g_r = construct_rna_graph_3d(path='data/1ASY_l_u.pdb')
+
+p_1 = plotly_rna_structure_graph(g_r)
+p_1.show()
