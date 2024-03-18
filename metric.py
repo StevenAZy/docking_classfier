39a4dc756782eea7d8a19515620009932e2653fe
StevenAZy <1315657491@qq.com>
2024-03-18 14:15:08 +0800

    add roc and use rnabert

--- metric.py
+++ metric.py
@@ -1,4 +1,6 @@
-from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import confusion_matrix, roc_curve, auc, RocCurveDisplay
 
 def calculate_metrics(labels, preds):
         cm = confusion_matrix(labels, preds)
@@ -9,3 +11,11 @@ def calculate_metrics(labels, preds):
         f1 = 2 * (precision * recall) / (precision + recall)
 
         return accuracy, precision, recall, f1, cm
+
+
+def roc_auc_plt(labels, preds):
+        fpr, tpr, thresholds = roc_curve(labels, preds)
+        roc_auc = auc(fpr, tpr)
+        display = RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc, estimator_name='example estimator')
+        display.plot()
+        plt.savefig('plt/roc.png')
